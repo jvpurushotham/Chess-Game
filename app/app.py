c07200ad6d1cd18d600953ef4ac386232381c26f
J V Purushotham <143670698+jvpurushotham@users.noreply.github.com>
2025-06-23 12:04:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -371,5 +371,8 @@ def reset():
     return jsonify({'message': 'Game reset'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
